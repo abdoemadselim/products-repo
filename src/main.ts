cde6d24cada36c2b8fc2b2d999ef2dd3666a37ed
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-08 15:37:49 +0300

    adjust cors for local development

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,24 @@ import express from "express"
 import { logger } from "#lib/logger/logger.js";
 import routes from "#features/routes.js"
 import bodyParser from "body-parser";
+import cors from "cors"
 
 const app = express();
+const allowedOrigins = [
+  "https://mukhtasar.pro", // For Frontend
+  "https://www.mukhtasar.pro", // For Frontend
+  "https://api.mukhtasar.pro", // For swagger
+  "http://localhost:3002",
+];
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
 app.use(bodyParser.json())
 app.use(routes)
 
